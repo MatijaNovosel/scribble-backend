f77b097953787bc2d4b561bb26d5c9c25c4660ca
Matija Novosel <mnovosel5@gmail.com>
2022-05-18 17:17:55 +0200

    Socket implementation

--- src/config/express.ts
+++ src/config/express.ts
@@ -2,14 +2,12 @@ import express from "express";
 import cors from "cors";
 import errorHandler from "../middleware/errorHandler";
 
-const createServer = (): express.Application => {
+const createApp = (): express.Application => {
   const app = express();
 
   app.use(express.urlencoded({ extended: true }));
   app.use(cors());
   app.use(express.json());
-
-  app.disable("x-powered-by");
 
   app.get("/test", (_req, res) => {
     res.send({
@@ -17,9 +15,13 @@ const createServer = (): express.Application => {
     });
   });
 
+  app.get("/", (_req, res) => {
+    res.send("Hello!");
+  });
+
   app.use(errorHandler);
 
   return app;
 };
 
-export default createServer;
+export default createApp;
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,29 @@
-import moduleAlias from "module-alias";
-import constants from "./constants";
-
-const sourcePath = constants.sourcePath;
-
-moduleAlias.addAliases({
-  "@middleware": `${sourcePath}/middleware`,
-  "@config": `${sourcePath}/config`
-});
-
-import createServer from "@config/express";
-import http from "http";
+import createApp from "@config/express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 import "dotenv/config";
 
-const host = process.env.HOST;
 const port = process.env.PORT;
 
-const startServer = async () => {
-  const app = createServer();
-  const server = http.createServer(app).listen({ host, port }, () => {
-    console.log(`Serving at: http://${host}:${port}`);
-  });
-  const signalTraps: NodeJS.Signals[] = ["SIGTERM", "SIGINT", "SIGUSR2"];
-  signalTraps.forEach((type) => {
-    process.once(type, async () => {
-      console.log(`process.once ${type}`);
-      server.close(() => {
-        console.log("HTTP server closed!");
+const startServer = () => {
+  const app = createApp();
+  const server = createServer(app);
+  const io = new Server(server);
+
+  io.on("connection", (socket) => {
+    console.log(`User connected: ${socket.id}`);
+    socket.on("test", () => {
+      io.emit("test-emit", {
+        test: 1
       });
     });
+    socket.on("disconnected", () => {
+      console.log(`User disconnected: ${socket.id}`);
+    });
+  });
+
+  server.listen(port, () => {
+    console.log(`Serving on address: http://localhost:${port}`);
   });
 };
 
