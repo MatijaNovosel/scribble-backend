2df708c5cced4e030be28a8bf76949cd0ed1a98d
Matija Novosel <mnovosel5@gmail.com>
2022-05-19 16:11:23 +0200

    Canvas sync wip

--- src/models/line.ts
+++ src/models/line.ts
@@ -1,5 +1,10 @@
 import { Offset } from "./offset";
 
+export interface LineFinishedData {
+  lines: Line[];
+  socketId: string;
+}
+
 export interface Line {
   width: number;
   color: string;
--- src/socket/drawing/drawingHandler.ts
+++ src/socket/drawing/drawingHandler.ts
@@ -1,9 +1,11 @@
-import { Line } from "@server/models/line";
+import { LineFinishedData } from "@server/models/line";
 import { Socket } from "socket.io";
 import eventTypes from "./eventTypes";
 
 export const drawingHandler = (socket: Socket) => {
-  socket.on(eventTypes.eventFinished, (data: Line[]) => {
-    socket.emit(eventTypes.updateCanvas, data);
+  socket.on(eventTypes.lineFinished, (data: LineFinishedData) => {
+    if (data.socketId !== socket.id) {
+      socket.emit(eventTypes.updateCanvas, data);
+    }
   });
 };
